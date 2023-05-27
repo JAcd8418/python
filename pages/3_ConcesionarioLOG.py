8400cf3bb0bc54dd9cab72257b644211b863bcb5
jacd8418 <jcortesda@cesde.net>
2023-05-26 23:02:42 -0500

    Actualizacion Concesionario

--- pages/3_ConcesionarioLOG.py
+++ pages/3_ConcesionarioLOG.py
@@ -19,49 +19,70 @@ st.title("LOG Pandas")
 #Seleccionar todas las filas de la columna "marca"
 st.subheader(" Listado de las Marcas")
 marcas = df_autos.loc[:, 'Marca']
+cantidad = marcas.shape[0]
+st.write("El total de Autos es: ", cantidad)
 st.dataframe(marcas)
 
 #Seleccionar las filas de los autos cuyo precio es mayor a $40,000
 st.subheader(" Autos con precio mayor a $ 40.000")
 precios = df_autos.loc[df_autos['Precio'] > 40000, :]
+cantidad = precios.shape[0]
+st.write("El total de Autos es: ", cantidad)
 st.dataframe(precios)
+
 
 #Seleccionar las filas de los autos que son de la marca "BMW".
 st.subheader(" Autos de la marca BMW")
 bmw = df_autos.loc[df_autos['Marca'] == 'BMW', :]
+cantidad = bmw.shape[0]
+st.write("El total de Autos es: ", cantidad)
 st.dataframe(bmw)
 
 #Seleccionar las filas de los autos que son de la marca "Toyota" y tienen un precio menor a $20,000
 st.subheader(" Autos de la marca BMW Toyota con precio menor a $20,000")
 toyota = df_autos.loc[(df_autos['Marca'] == 'Toyota') & (df_autos['Precio'] < 20000), :]
+cantidad = toyota.shape[0]
+st.write("El total de Autos es: ", cantidad)
 st.dataframe(toyota)
 
 #Seleccionar las filas de los autos que son del año 2019
 st.subheader(" Autos del año 2019")
 modelo = df_autos.loc[df_autos['Año'] == 2019, :]
+cantidad = modelo.shape[0]
+st.write("El total de Autos es: ", cantidad)
 st.dataframe(modelo)
 
 #Seleccionar las filas de los autos que son del año 2016 o anteriores.
 st.subheader(" Autos del año 2016 o anteriores")
 modelo = df_autos.loc[df_autos['Año'] <= 2016, :]
+cantidad = modelo.shape[0]
+st.write("El total de Autos es: ", cantidad)
 st.dataframe(modelo)
 
 #Seleccionar las filas de los autos que son de la marca "Honda" y el modelo es "Civic".
 st.subheader(" Autos Honda Civic")
 hondaCivic = df_autos.loc[(df_autos['Marca'] == 'Honda') & (df_autos['Modelo'] == 'Civic'), :]
+cantidad = hondaCivic.shape[0]
+st.write("El total de Autos es: ", cantidad)
 st.dataframe(hondaCivic)
 
 #Seleccionar las filas de los autos que tienen un precio entre $25,000 y $30,000.
 st.subheader(" Autos con precios entre $25.000 y $30.000")
 precios = df_autos.loc[(df_autos['Precio'] > 25000) & (df_autos['Precio'] <= 30000) , :]
+cantidad = precios.shape[0]
+st.write("El total de Autos es: ", cantidad)
 st.dataframe(precios)
 
 #Seleccionar las filas de los autos que tienen un precio mayor a $30,000 y el modelo es "C-Class"
 st.subheader(" Autos con precios mayor a $30.000 y modelo es C-Class")
 preciosmodelo = df_autos.loc[(df_autos['Precio'] > 30000) & (df_autos['Modelo'] == 'C-Class') , :]
+cantidad = preciosmodelo.shape[0]
+st.write("El total de Autos es: ", cantidad)
 st.dataframe(preciosmodelo)
 
 #Seleccionar las filas de los autos que son de la marca "Volkswagen" y el modelo no es "Jetta".
 st.subheader(" Autos Volkswagen Jetta")
 volkwagenjetta = df_autos.loc[(df_autos['Marca'] == 'Volkswagen') & (df_autos['Modelo'] == 'Jetta'), :]
+cantidad = volkwagenjetta.shape[0]
+st.write("El total de Autos es: ", cantidad)
 st.dataframe(volkwagenjetta)
--- pages/4_ConcesionarioILOG.py
+++ pages/4_ConcesionarioILOG.py
@@ -20,29 +20,62 @@ st.title("I-LOG Pandas")
 #Seleccionar las filas de los autos de los primeros 5 fabricantes en el DataFrame.
 st.subheader(" Autos de los 5 primeros fabricantes")
 dato = df_autos.iloc[0:5, :]
+cantidad = dato.shape[0]
+st.write("El total de Autos es: ", cantidad)
 st.dataframe(dato)
 
 #Seleccionar las filas de los autos de los últimos 5 fabricantes en el DataFrame
 st.subheader(" Autos de los 5 ultimos fabricantes")
 dato = df_autos.iloc[5:10, :]
+cantidad = dato.shape[0]
+st.write("El total de Autos es: ", cantidad)
 st.dataframe(dato)
 
 #Seleccionar la primera columna de todas las filas del DataFrame
 st.subheader("Listado de las Marcas ")
 dato = df_autos.iloc[:, 0 ]
+cantidad = dato.shape[0]
+st.write("El total de Autos es: ", cantidad)
 st.dataframe(dato)
 
 #Seleccionar las filas pares del DataFrame
 st.subheader("las filas pares ")
 pares = df_autos.iloc[::2, :]
+cantidad = pares.shape[0]
+st.write("El total de Autos es: ", cantidad)
 st.dataframe(pares)
 
 #Seleccionar las filas impares del DataFrame que tienen un precio mayor a $25,000
 st.subheader("las filas impares con precio mayos a $25.000 ")
 impares = df_autos.iloc[1::2, :][df_autos['Precio'] > 25000]
+cantidad = impares.shape[0]
+st.write("El total de Autos es: ", cantidad)
 st.dataframe(impares)
 
 #Seleccionar las filas de los autos que son de la marca "Ford" y el modelo es "F-150"
 st.subheader("Autos Ford F-150")
 dato = df_autos.iloc[2::1][df_autos['Modelo']=='F-150']
+cantidad = dato.shape[0]
+st.write("El total de Autos es: ", cantidad)
 st.dataframe(dato)
+
+#Seleccionar las filas de los autos que son del año 2018 y tienen un precio mayor a $20,000
+st.subheader("Autos del Año 2018 con precio mayor a $20,000")
+age= df_autos.iloc[ :: ][df_autos['Precio'] >= 2000][df_autos['Año']==2018]
+cantidad = age.shape[0]
+st.write("El total de Autos es: ", cantidad)
+st.dataframe(age)
+
+#Seleccionar las filas de los autos que tienen un precio mayor a $30,000 y la marca es "Toyota".
+st.subheader("Autos Toyota con precio mayor a $30,000")
+age= df_autos.iloc[ :: ][df_autos['Precio'] >= 3000][df_autos['Marca']=='Toyota']
+cantidad = age.shape[0]
+st.write("El total de Autos es: ", cantidad)
+st.dataframe(age)
+
+#Seleccionar las filas de los autos que son de la marca "Honda" y el modelo no es "Civic".
+st.subheader("Autos Honda Civic")
+age= df_autos.iloc[ :: ][df_autos['Marca']=='Honda'][df_autos['Modelo'] == 'Civic']
+cantidad = age.shape[0]
+st.write("El total de Autos es: ", cantidad)
+st.dataframe(age)
